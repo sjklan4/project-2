c72d23673ead4e34dfd0571fd6efb285a8c2bcbc
sjklan <dpeltms4@gmail.com>
2023-06-14 16:59:26 +0900

    0614 model userinfo 추가작업

--- project/app/Models/UserInfo.php
+++ project/app/Models/UserInfo.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserInfo extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'user_info';
+
+    protected $primery = 'user_id';
+
+    protected $garded = ['user_id'];
+
+    protected $dates = ['deleted_at'];  
 }
