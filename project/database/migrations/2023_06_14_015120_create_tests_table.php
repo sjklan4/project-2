b9d3c005901582320ea283b024ebf383469f0708
chaesuji <suji0251@naver.com>
2023-06-14 11:07:26 +0900

    푸시 테스트

--- project/database/migrations/2023_06_14_015120_create_tests_table.php
+++ project/database/migrations/2023_06_14_015120_create_tests_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('tests', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            //test
         });
     }
 
