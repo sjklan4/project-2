6afea4b6bfb5fb86382803cdfcc0e88b04df7800
choooii <dkfks0832@gmail.com>
2023-06-15 10:54:57 +0900

    0615 blade update

--- project/resources/views/boardList.blade.php
+++ project/resources/views/boardList.blade.php
@@ -3,7 +3,22 @@
 @section('title', 'List')
 
 @section('contents')
-    <a href="{{route('boards.create')}}">작성하기</a>
+    @section('boardNav')
+        <!-- As a link -->
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+            </div>
+        </nav>
+        
+        <!-- As a heading -->
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">Navbar</span>
+            </div>
+        </nav>
+    @show
+    <a>작성하기</a>
     <table>
         <tr>
             <th>글번호</th>
@@ -12,15 +27,6 @@
             <th>등록일</th>
             <th>수정일</th>
         </tr>
-    @forelse($data as $item)
-        <tr>
-            <td>{{$item->id}}</td>
-            <td></td>
-            <td>{{$item->hits}}</td>
-            <td>{{$item->created_at}}</td>
-            <td>{{$item->updated_at}}</td>
-        </tr>
-    @empty
         <tr>
             <td></td>
             <td>게시글 없음</td>
@@ -28,6 +34,5 @@
             <td></td>
             <td></td>
         </tr>
-    @endforelse
     </table>
 @endsection
