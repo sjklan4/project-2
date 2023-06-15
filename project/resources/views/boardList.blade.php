04b8b55b6150928928e60cd656284d129399beb3
choooii <dkfks0832@gmail.com>
2023-06-15 10:39:02 +0900

    0615 blade layout up

---
+++ project/resources/views/boardList.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.layout')
+
+@section('title', 'List')
+
+@section('contents')
+    <a href="{{route('boards.create')}}">작성하기</a>
+    <table>
+        <tr>
+            <th>글번호</th>
+            <th>글제목</th>
+            <th>조회수</th>
+            <th>등록일</th>
+            <th>수정일</th>
+        </tr>
+    @forelse($data as $item)
+        <tr>
+            <td>{{$item->id}}</td>
+            <td></td>
+            <td>{{$item->hits}}</td>
+            <td>{{$item->created_at}}</td>
+            <td>{{$item->updated_at}}</td>
+        </tr>
+    @empty
+        <tr>
+            <td></td>
+            <td>게시글 없음</td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    @endforelse
+    </table>
+@endsection
