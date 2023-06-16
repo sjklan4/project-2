0326a94246eb2b9aab273e4ab50d8f4f7349296e
kbj95 <gwonbongjeong@gmail.com>
2023-06-16 12:13:40 +0900

    0616 route

---
+++ project/public/js/home.js
@@ -0,0 +1,9 @@
+let calendar = document.getElementById("calendar").value;
+
+// calendar.value = new Date().toISOString().substring(0,10);
+
+function chkDate(){
+    let calendar = document.getElementById("calendar").value;
+
+    console.log(calendar);
+}
