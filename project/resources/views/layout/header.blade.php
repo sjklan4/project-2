18ddf8722aaa34b1f656e0dca76b885db7bc86a3
choooii <dkfks0832@gmail.com>
2023-06-20 18:21:53 +0900

    0620 헤더 수정

--- project/resources/views/layout/header.blade.php
+++ project/resources/views/layout/header.blade.php
@@ -1,14 +1,82 @@
-<h2>Header</h2>
-
-{{-- 로그인 상태 --}}
-@auth
-    <button type="button" onclick="location.href='{{route('user.logout')}}'">로그아웃</button>
-    <button type="button" onclick="location.href='{{route('user.userinfoedit')}}'">회원정보수정</button>
-@endauth
-
-{{-- 로그아웃 상태 --}}
-@guest
-<button type="button" onclick="location.href='{{route('user.login')}}'">로그인</button>
-@endguest
-
-<hr> 
+<section class="navigation">
+    <div class="nav-container">
+        <div class="brand">
+            <a href="{{route('home')}}">Home</a>
+        </div>
+        <nav>
+            <div class="nav-mobile"><a id="navbar-toggle" href="#!"><span></span></a></div>
+            <ul class="nav-list">
+                <li>
+                    <a href="#">My Food</a>
+                    <ul class="navbar-dropdown">
+                        <li>
+                            <a href="#!">기록</a>
+                        </li>
+                        <li>
+                            <a href="#!">자주먹는 식단</a>
+                        </li>
+                        <li>
+                            <a href="{{route('food.index')}}">등록한 음식</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#!">Board</a>
+                    <ul class="navbar-dropdown">
+                        <li>
+                            <a href="{{route('board.indexNum', ['board' => 1])}}">건강관리</a>
+                        </li>
+                        <li>
+                            <a href="{{route('board.indexNum', ['board' => 2])}}">다이어트</a>
+                        </li>
+                        <li>
+                            <a href="{{route('board.indexNum', ['board' => 3])}}">10대</a>
+                        </li>
+                        <li>
+                            <a href="{{route('board.indexNum', ['board' => 4])}}">20대</a>
+                        </li>
+                        <li>
+                            <a href="{{route('board.indexNum', ['board' => 5])}}">30대</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#!">My Page</a>
+                    <ul class="navbar-dropdown">
+                        <li>
+                            <a href="#!">나의정보</a>
+                        </li>
+                        <li>
+                            <a href="#!">비밀번호 변경</a>
+                        </li>
+                        <li>
+                            <a href="#!">식단 설정 변경</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#!">Challenge</a>
+                    <ul class="navbar-dropdown">
+                        <li>
+                            <a href="#!">퀘스트 수락</a>
+                        </li>
+                        <li>
+                            <a href="#!">퀘스트 관리</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    {{-- 로그인 상태 --}}
+                    @auth
+                    <button type="button" onclick="location.href='{{route('user.logout')}}'">로그아웃</button>
+                    @endauth
+        
+                    {{-- 로그아웃 상태 --}}
+                    @guest
+                    <button type="button" onclick="location.href='{{route('user.login')}}'">로그인</button>
+                    @endguest
+                </li>
+            </ul>
+        </nav>
+    </div>
+</section>
--- project/resources/views/layout/layout.blade.php
+++ project/resources/views/layout/layout.blade.php
@@ -8,16 +8,18 @@
     <link rel="stylesheet" href="{{asset('css/common.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{asset('css/header.css')}}">
     @section('css')
     @show
 </head>
-{{-- <body  data-url="{{ url('user/chdeckEmail') }}" data-csrf="{{ csrf_token() }}"> --}}
+<body>
+    {{-- data-url="{{ url('user/chdeckEmail') }}" data-csrf="{{ csrf_token() }}"> --}}
     @include('layout.header')
     @yield('contents')
     @include('layout.footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="{{asset('js/common.js')}}"></script>
     @yield('js')
-
-
 </body>
 </html>
