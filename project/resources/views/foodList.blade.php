70a1a67bad5a773df8c4f1ebbb94e9cbf17f1bfe
chaesuji <suji0251@naver.com>
2023-06-24 16:01:56 +0900

    0624 search insert, css

--- project/resources/views/foodList.blade.php
+++ project/resources/views/foodList.blade.php
@@ -9,37 +9,50 @@
 @section('contents')
     <form action="{{route('search.list.get', ['id' => Auth::user()->user_id])}}" method="get" class="searchform">
         @csrf
-        <input type="text" name="search_input">
-        <button type="submit">검색</button>
+        <div class="searchform">
+        <input type="text" name="search_input" class="searchip" placeholder="검색할 단어를 입력하세요.">
+        <button type="submit" class="searchbtn"><i class="fa-solid fa-magnifying-glass" style="color: #000000;"></i></button>
+        </div>
     </form>
 
-    <ul>
+    <ul class="searchtab">
         <li class="tab1">
+            <i class="fa-solid fa-utensils" style="color: #ee6666;"></i>
             저장된 식단
         </li>
         <li class="tab2">
+            <i class="fa-solid fa-cart-shopping" style="color: #538e04;"></i>
             선택된 음식
         </li>
         <li class="tab3" onclick="location.href='{{route('food.create')}}'">
+            <i class="fa-solid fa-mortar-pestle" style="color: #6799e4;"></i>
             음식등록
         </li>
     </ul>
 {{-- todo : 사용자 등록/최근 먹은 음식 표시, 유효성 검사 --}}
 {{-- todo : 저장된 식단 페이징 / 검색 결과 더보기 또는 페이징 --}}
 {{-- todo : 선택된 음식 식단/음식 삭제 및 인분 수 조절 --}}
-{{-- todo : insert 기능 수행 --}}
-{{-- ? 검색/선택된 음식 인분 수 입력 및 설정 -> 첫 입력 값만 가져옴 : search.js --}}
 {{-- todo : 드롭다운 메뉴 -> 영양성분 표시 --}}
-
-{{-- !? 선택된 음식 새로고침 시 유지 --}}
-    <div>
+    <div class="searchContainer">
+        <hr>
         @if (!empty($foods))
             <div class="user_search">
                 @foreach ($foods as $item)
-                    <input type="checkbox" name="usercheck" id="usercheck" value="{{$item->food_id}}" onclick='getFoodValue({{Auth::user()->user_id}})'>
-                    <label for="usercheck" id="food_name">{{$item->food_name}}</label>
+                    <div class="searchhhh">
+                        <input type="checkbox" name="usercheck" id="usercheck" value="{{$item->food_id}}" onclick='getFoodValue({{Auth::user()->user_id}})'>
+                        <label for="usercheck" id="food_name">{{$item->food_name}}</label>
+                    
                     <span>인분 수 : </span>
                     <input type="number" name="userving" id="userving" min="0.5" max="100">
+                    @if (!empty($item->user_id))
+                        <span class="usercreate">사용자 등록</span>
+                    @endif
+                    @if (!empty($recent))
+                        
+                        {{-- <span>최근 먹은 음식</span> --}}
+                    @endif
+                    <hr class="div_hr">
+                </div>
                     <br>
                 @endforeach
                 @if ($foods->hasPages())
@@ -67,7 +80,7 @@
                 @endif
                     </div>
                 @else
-                    <p class="nosearch">검색어</p>
+                    <p class="nosearch">검색 결과가 없습니다.</p>
                 @endif
         
         <div class="fav_diets">
@@ -76,8 +89,10 @@
                     자주먹는 식단
                 </h2>
                 @foreach ($dietname as $names)
-                    <input type="checkbox" name="userdiet" id="userdiet" value="{{$names->fav_id}}" onclick='getDietValue()'>
-                    <span> {{$names->fav_name}}</span>
+                    <div class="fav_div">
+                        <input type="checkbox" name="userdiet" id="userdiet" value="{{$names->fav_id}}" onclick='getDietValue()'>
+                        <span> {{$names->fav_name}}</span>
+                    </div>
                     <br>
                     @foreach ($dietfood as $foods)
                     @if($foods->fav_id === $names->fav_id)
@@ -97,7 +112,7 @@
                 <p>asdsdf</p>
                 @endif
             
-                {{-- @if ($dietname->hasPages())
+                @if ($dietname->hasPages())
                 <ul class="pagination pagination">
                     @if ($dietname->currentPage() > 1)
                         <a href="{{ $dietname->previousPageUrl() }}"><span class="fa fa-chevron-left" aria-hidden="true">이전</span></a>
@@ -119,7 +134,7 @@
                         <span>이후</span>
                     @endif
                 </ul>
-                @endif --}}
+                @endif
             </div>
             
         <div class="user_select">
@@ -136,13 +151,15 @@
                 <br>
             @endif
                 <button type="button">취소</button>
-                <button type="button" onclick="location.href='{{route('search.insert', ['id' => Auth::user()->user_id, 'date' => '2023-06-22', 'time' => '1'])}}'">입력</button>
+                <button type="button" onclick="location.href='{{route('search.insert', 
+                ['id' => Auth::user()->user_id, 'date' => '2023-06-22', 'time' => '0'])}}'">입력</button>
         </div>
         
     </div>
 @endsection
 
 @section('js')
+    <script src="https://kit.fontawesome.com/8c69259d3d.js" crossorigin="anonymous"></script>
     <script src="{{asset('js/jquery.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('js/search.js')}}"></script>
 @endsection
