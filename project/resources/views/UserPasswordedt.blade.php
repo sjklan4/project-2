806025ba27ac32942daa39118f097963628fd5df
sjklan4 <dpeltms4@gmail.com>
2023-06-27 00:53:02 +0900

    0627ps수정

--- project/resources/views/UserPasswordedt.blade.php
+++ project/resources/views/UserPasswordedt.blade.php
@@ -3,22 +3,30 @@
 @section('title', 'passwordchange')
 
 @section('passwordcontents')
-    <form action="{{ route('user.userpseditpost') }}" method="post">
-        @csrf
-        <label for="password">기존비밀번호 : </label>
-        <input type="password" name="password" id="password" placeholder="비밀번호를 입력해 주세요">
-        <button type="button" id="passwordchk" disabled>확인</button>
-        <br>
-        <span id="writeerror"></span>
-        <br>
-        <label for="newpassword">새 비밀번호 : </label>
-        <input type="text" name="newpassword" id="newpassword">
-        <br>
-        <label for="newpasswordchk">새 비밀번호 확인: </label>
-        <input type="text" name="newpasswordchk" id="newpasswordchk">
-        <br>
-        <button type = "submit" id="passwordchg" disabled>변경</button>
-    </form>
+    <div class="findps-body">
+        <form action="{{ route('user.userpseditpost') }}" method="post">
+            @csrf
+        <div class="info-tablemenu">
+            <table class="findps-table">
+                <tr>
+                    <th class="passth" colspan="2"><label for="password">기존비밀번호 : </label></th>
+                    <td><input type="password" name="password" id="password" placeholder="비밀번호를 입력해 주세요"></td>
+                    <td><button type="button" id="passwordchk" disabled>확인</button></td>
+                </tr>
+                <span id="writeerror"></span>
+                <tr>
+                    <th class="passth"  colspan="2"><label for="newpassword">새 비밀번호 : </label></th>
+                    <td><input type="text" name="newpassword" id="newpassword"></td>
+                </tr>
+                <tr>
+                    <th class="passth"  colspan="2"><label for="newpasswordchk">새 비밀번호 확인 : </label></th>
+                    <td><input type="text" name="newpasswordchk" id="newpasswordchk"></td>
+                </tr>
+                <caption class="pscap"><button type = "submit" id="passwordchg" disabled>변경</button></caption>
+            </table>
+            </form>
+        </div>
+    </div>
 @endsection
 
 @section('js')
