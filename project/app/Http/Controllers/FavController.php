eac8d6d1a819a13f0d981309aa303c3947f0a062
sjklan <dpeltms4@gmail.com>
2023-06-30 11:33:24 +0900

    0630주석작업

--- project/app/Http/Controllers/FavController.php
+++ project/app/Http/Controllers/FavController.php
@@ -35,19 +35,22 @@ class FavController extends Controller
 
         // exit;
 
+        //빈배열을 준비 : 아래 조건에 맞는 food_name 정보를 배열로 받아서 넣을 준비용
         $arr = [];
-        for ($i=0; $i < $favname->count(); $i++) { 
-            $favfood = FoodInfo::select('food_infos.food_name')
-                            ->join('fav_diet_food','food_infos.food_id','fav_diet_food.food_id')
-                            ->join('fav_diets','fav_diet_food.fav_id','fav_diets.fav_id')
-                            ->where('fav_diet_food.fav_id', $favname[$i]->fav_id)
+        // favname에 담겨 있는 데이터 수 만큼 반복 문을 진행 한다.
+        for ($i=0; $i < $favname->count(); $i++) {  
+            $favfood = FoodInfo::select('food_infos.food_name') // food_infos테이블에서 food_name을 찾는다
+                            ->join('fav_diet_food','food_infos.food_id','fav_diet_food.food_id') //fav_diet_food 테이블의 food_id와 food_infos의 food_id를 연결 해서 같은 food_id에 있는 food_name을 찾는다.
+                            ->join('fav_diets','fav_diet_food.fav_id','fav_diets.fav_id') // fav_diets테이블의 fav_id와 fav_diet_food의 fav_id와 같은 값을 찾는다.
+                            ->where('fav_diet_food.fav_id', $favname[$i]->fav_id) //위에서 받은 favname의 i번째 fav_id를 조회 한다.(ex : fav_id안에 40번 유저의 3,4,5식단이 들어 있을시 3번 식단의 음식 이름들을 전부 조회해서 가져오고 그다음 4번, 5번 식으로 반복해서 음식 이름들을 각각 전부 가져온다.)
                             ->whereNull('fav_diet_food.deleted_at')
                             ->get();
+            // $favfood에 들어온 모든 값들을 빈배열에 담는다.
             $arr[] = $favfood;
             
         }
     
-    
+    //
         if($id > 0){
             $foodinfo = FoodInfo::select('fav_diet_food.fav_f_id','food_infos.food_id','fav_diet_food.fav_f_intake','food_infos.food_name', 'food_infos.kcal', 'food_infos.carbs', 'food_infos.protein', 'food_infos.fat', 'food_infos.sugar', 'food_infos.sodium')
                                     ->join('fav_diet_food','fav_diet_food.food_id','food_infos.food_id')
--- project/resources/views/favdiet.blade.php
+++ project/resources/views/favdiet.blade.php
@@ -23,10 +23,12 @@
                         @for($i = 0; $i < count($favname); $i++)
                         <th>
                             {{-- 삭제플레그를 위해서 fav_id값을 hidden처리해서 가져옴 --}}
+                            {{-- 로그인 계정의 fav_id와 fav_name을 반복해서 출력 fav_name은 a링크로 식단안에 있는 음식정보를 표시 하기 위함 --}}
                             <span hidden>{{$favname[$i]->fav_id}}</span>
                             <a href="{{route('fav.favfoodinfo', ['fav_id' => $favname[$i]->fav_id])}}" class ="favdietname">{{$favname[$i]->fav_name}}</a>
                         </th>
                         <td>
+                            {{-- favfood - 음식이름도 같은 방식으로 count만큼 반복 출력 --}}
                             @foreach ($favfood[$i] as $item)
                             <span>{{$item->food_name}}</span>
                             @endforeach
@@ -37,13 +39,20 @@
             </div>
 
             <div class="foodnutri">
+            {{-- 자주 찾는 식단의 인분(먹은양)수정을 위한 form테그 부분 --}}
                 <form action="{{route('fav.intakeupdate')}}" method="post">
                     @csrf
+            {{-- foodinfo 데이터가 있으면...... --}}
                 @if (isset($foodinfo))
+
                     @foreach ($foodinfo as $item)
-                        {{-- food_id를 input에 담아서 배열로 보내기 위해서 name을 빈배열식으로 주고 hidden으로 받은 food_id 값을 보내준다. 배열로 되어 있는 이유는 지금 받아오는 값들이 배열형식의 다수의 값을 가져 오는 foreach구문이기 때문에 다수의 값들을 한가지 input에 담아서 배열로 보내주기 위해 food_id[]로 만들어야 한다. 지금 오는 값은 food_id=[123,124,2943]식으로 여러개가 오는중 --}}
+                    {{-- foodinfo데이터에 있는 값들을 배열로 받아와 반복해서 출력. --}}
+                    {{-- food_id를 input에 담아서 배열로 보내기 위해서 name을 빈배열식으로 주고 hidden으로 받은 food_id 값을 보내준다. 배열로 되어 있는 이유는 지금 받아오는 값들이 배열형식의 다수의 값을 가져 오는 foreach구문이기 때문에 다수의 값들을 한가지 input에 담아서 배열로 보내주기 위해 food_id[]로 만들어야 한다. 지금 오는 값은 food_id=[123,124,2943]식으로 여러개가 오는중 --}}
+                    {{-- 아래 input은 food_id에 해당하는 섭취량 수정을 위해 food_id값들을 배열로 받아와서 그 내용들중 하나를 수정하기 위한 준비를 시키는 구문 food_id는 안에 각 영양정보들이 같이 배열로 들어 있어서 그 값들을 한번에 가져와야 된다. 그리고 나서 바꾸려고 하는 값으(테이블의 컬럼)을 input으로 지정해서 변경 시키도록 한다. >input안에 input을 사용해서 수정하도록함 hidden을 주어서 food_id값을 수정하지 못하도록 만듬 --}}
                         <input type="hidden" name="food_id[]" value="{{$item->food_id}}">
+                        
                         <span hidden>{{$item->fav_f_id}}</span>
+
                         <span hidden>{{$item->food_id}}</span>
                     <table>
                         <tr>
@@ -69,8 +78,12 @@
                                 <span>나트륨 : {{$item->sodium}}</span>
                             </td>                   
                                 <label for="intake">
-                            <td><input type="number" name="intake[]" id="intake" value={{$item->fav_f_intake}}  required>인분</label>
+
+                            <td>
+                                {{-- 인분수 수정값을 입력하기 위한 구문 intake[]를 배열로 해서 여러 값들이 한번에 들어 갈수 있도록 함. 기존 섭취 량을 받아오기 위해서{{$item->fav_f_intake}} 를 사용하고 required를 주어서 필수 입력값으로 설정  --}}
+                                <input type="number" name="intake[]" id="intake" value={{$item->fav_f_intake}}  required>인분</label>
                             </td>
+
                             <td><span class="del-food" id="del-food">
                                     <a href="{{route('fav.del', $item->fav_f_id)}}" class="del-food">X</a>
                                 <p class="arrow_box">등록하신 음식이 삭제 됩니다.</p></span>
@@ -78,6 +91,7 @@
                             </tr>
                     </table>
                     @endforeach
+
                     <div class="submitbtn">
                         <button type="button" onclick="location.href='{{route('fav.delete', $id)}}'" id="greenBtn">삭  제</button>
                         <button type="submit" id="greenBtn">수  정</button>
@@ -87,6 +101,7 @@
         </div>    
                     
                 </form>
+                {{-- form문 끝부분! --}}
         <div></div>
     </div>
 </div>
