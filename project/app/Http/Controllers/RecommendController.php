75cb24afac18721414d81de8c75335015fd3c202
chaesuji <suji0251@naver.com>
2023-07-12 18:11:28 +0900

    식단 추천 블레이드, 컨트롤러 생성 및 라우터 수정

---
+++ project/app/Http/Controllers/RecommendController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecommendController extends Controller
+{
+    public function pview() {
+        return view('recommend');
+    }
+
+    
+}
---
+++ project/resources/views/recommend.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.layout')
+
+@section('title', 'FoodSearch')
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/search.css')}}">
+@endsection
+
+@section('contents')
+    aaaa
+@endsection
+
+@section('js')
+
+@endsection
