6888d01015ef31a51e111473910686e05e5d07b6
chaesuji <suji0251@naver.com>
2023-07-14 02:15:38 +0900

    식단 추천 레이아웃 구성

--- project/app/Http/Controllers/RecommendController.php
+++ project/app/Http/Controllers/RecommendController.php
@@ -9,10 +9,14 @@ use Illuminate\Support\Facades\Auth;
 class RecommendController extends Controller
 {
     public function pview() {
-        return view('recommend');
+        $id = Auth::user()->user_id;
+        $userinfo = KcalInfo::select('goal_kcal')
+        ->where('user_id', $id)
+        ->get();
+        return view('recommend')->with('userinfo', $userinfo);
     }
 
-    public function recommned() {
+    public function recommned(Request $req) {
         // 유저 id 획득
         $id = Auth::user()->user_id;
 
@@ -20,15 +24,32 @@ class RecommendController extends Controller
         ->where('user_id', $id)
         ->get();
 
-
         // ! 탄수 1g : 4kcal | 단백질 1g : 4kcal | 지방 : 1g : 9kcal
         // 유지칼로리 계산
-        if($result[0]['user_activity'] == 0){
-            $tdee = $result[0]['goal_kcal'] * 1.375;
-        }else if($result[0]['user_activity'] == 1){
-            $tdee = $result[0]['goal_kcal'] * 1.55;
-        }else{
-            $tdee = $result[0]['goal_kcal'] * 1.725;
-        };
+        if($result){ // 목표 칼로리 데이터가 설정되어 있는 경우
+            if($result[0]['user_activity'] == 0){
+                $tdee = $result[0]['goal_kcal'] * 1.375;
+            }else if($result[0]['user_activity'] == 1){
+                $tdee = $result[0]['goal_kcal'] * 1.55;
+            }else{
+                $tdee = $result[0]['goal_kcal'] * 1.725;
+            };
+        }else{ // 목표 칼로리 데이터가 설정되어 있지 않은 경우
+            if($req->gender == 1){ // 남자
+                $bmr = round(
+                    66 
+                    + (13.7 * (int)$req->weight) 
+                    + (5 * (int)$req->height) 
+                    - (6.8 * $req->age)
+                );
+            }else{ // 여자
+                $bmr = round(
+                    655 
+                    + (9.6 * (int)$req->weight) 
+                    + (1.7 * (int)$req->height) 
+                    - (4.7 * $req->age)
+                );
+            }
+        }
     }
 }
--- project/resources/views/recommend.blade.php
+++ project/resources/views/recommend.blade.php
@@ -9,12 +9,52 @@
 @section('contents')
     <div>
         <h1>식단 추천</h1>
-        <div>
-            {{-- <h2>어떤 유형</h2> --}}
-            <button>감량</button>
-            <button>증량</button>
-            <button>건강식</button>
-        </div>
+        @if (!userinfo)
+            <form action="{{route('recommend')}}" method="get">
+                <span>식단 유형 > </span>
+                <input type="checkbox" name="dietcate" id="loseweight" value="0">
+                <label for="loseweight">감량</label>
+                <input type="checkbox" name="dietcate" id="increase" value="1">female
+                <label for="increase">증량</label>
+                <input type="checkbox" name="dietcate" id="health" value="2">
+                <label for="health">일반</label>
+                <br>
+                <p>성별</p>
+                <label for="male">남자</label>
+                <input type="radio" name="gender" id="male" value="0">
+                <label for="female">여자</label>
+                <input type="radio" name="gender" id="female" value="1">
+                <p>키</p>
+                <input type="text" name="height" id="height">
+                <p>몸무게</p>
+                <input type="text" name="weight" id="weight">
+                <p>나이</p>
+                <input type="text" name="age" id="age">
+                <br>
+                <p>활동량</p>
+                <input type="checkbox" name="activity" id="lessact" value="0">
+                <label for="lessact">적음</label>
+                <input type="checkbox" name="activity" id="nomalact" value="1">female
+                <label for="nomalact">보통</label>
+                <input type="checkbox" name="activity" id="moeract" value="2">
+                <label for="moeract">많음</label></label>
+                <br>
+                <button type="submit">추천받기</button>
+            </form>
+        @else
+        <form action="{{route('recommend')}}" method="get">
+            <span>식단 유형 > </span>
+            <input type="checkbox" name="dietcate" id="loseweight" value="0">
+            <label for="loseweight">감량</label>
+            <input type="checkbox" name="dietcate" id="increase" value="1">female
+            <label for="increase">증량</label>
+            <input type="checkbox" name="dietcate" id="health" value="2">
+            <label for="health">일반</label>
+            <br>
+            <button type="submit">추천받기</button>
+        </form>
+        @endif
+        
         {{-- <hr>
         <div>
             <h2>추천 음식</h2>
@@ -23,9 +63,7 @@
         <hr>
         <div>
             <h2>총 영양성분</h2>
-        </div>
-        <button></button>
-        <button></button> --}}
+        </div> --}}
     </div>
 @endsection
 
