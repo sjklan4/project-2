aed3b42216e42df583afdf5bf065628e2f7b4d2b
choooii <dkfks0832@gmail.com>
2023-07-18 13:16:08 +0900

    0718 회원가입 수정

--- project/resources/views/regist.blade.php
+++ project/resources/views/regist.blade.php
@@ -15,8 +15,8 @@
                 <div class="email">
                     <label for="user_email">이메일</label>
                     <br>
-                    <input type="text" name="user_email" id="user_email"  value="{{ $errors->has('user_email') ? '' : old('user_email', isset($data) ? $data->user_email : '') }}" placeholder="인증을 진행할 이메일로 입력해주세요" autocomplete="off" required>
-                    <button type="button" id="checkEmail" class = "greenBtn ms-1 ms-sm-4" onclick="duplicationEmail();">중복체크</button>
+                    <input type="text" name="user_email" id="user_email"  value="{{ $errors->has('user_email') ? '' : old('user_email', isset($data) ? $data->user_email : '') }}" autocomplete="off" required>
+                    {{-- <button type="button" id="checkEmail" onclick="duplicationEmail();">중복체크</button> --}}
                     <div id="mailMsg"></div>
                     @error('user_email')
                         <div class="text-danger">{{ $message }}</div>
