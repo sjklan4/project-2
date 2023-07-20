4ac281cf748fe36fa01a5d73bb90374030e81544
chaesuji <suji0251@naver.com>
2023-07-21 02:07:49 +0900

    쿼리 수정

--- project/app/Http/Controllers/ReportController.php
+++ project/app/Http/Controllers/ReportController.php
@@ -9,21 +9,27 @@ class ReportController extends Controller
 {
     // v002 add 게시글 및 댓글 신고
     public function report(Request $req) {
-        // 이때까지 한 신고를 모아 볼 수 있는 페이지가 있어야 할 거 같은 느낌...
-    
         $rep_r_id = DB::table('report_reasons')->insertGetId([
             'rep_r_content' => $req->reporttext,
             'rep_flg' => $req->reportselect
         ]);
 
-        DB::table('report_lists')->insert([
+        $insert = DB::table('report_lists')->insertGetId([
             'reporter' => $req->reporter,
             'suspect' => $req->suspect,
             'board_id' => $req->board_id,
             'reply_id' => $req->reply_id,
             'rep_r_id' => $rep_r_id,
-            'complate_flg' => 0
+            'complate_flg' => 0,
+            'created_at' => now()
         ]);
+
+        // 확실하지 않은 쿼리
+        $getSuspectId = DB::table('report_lists')
+        ->select('report_lists.suspect', 'user_infos.report_num')
+        ->join('user_infos', 'user_infos.user_id', 'report_lists.suspect')
+        ->where('rep_id', $insert)
+        ->get();
 
         return redirect()->route('home');
     }
