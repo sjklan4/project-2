504293691a4f113dabf5cf9075dc56b55c22cd1d
chaesuji <suji0251@naver.com>
2023-07-20 10:41:24 +0900

    병합

--- project/app/Providers/HeaderViewComposerServiceProvider.php
+++ project/app/Providers/HeaderViewComposerServiceProvider.php
@@ -28,13 +28,17 @@ class HeaderViewComposerServiceProvider extends ServiceProvider
     {
         View::composer('*', function ($view) {
             // 유저 알람 정보 획득
-            $user_id = Auth::user()->user_id;
-            $alarm = Alarm::where('user_id', $user_id)
-                ->where('alarm_flg', '0')
-                ->get();
+            if(Auth::user()) {
+                $user_id = Auth::user()->user_id;
+                $alarm = Alarm::where('user_id', $user_id)
+                    ->where('alarm_flg', '0')
+                    ->get();
 
-            // 뷰에 데이터 전달
-            $view->with('alarmData', $alarm[0]);
+                if($alarm->count() > 0) {
+                    // 뷰에 데이터 전달
+                    $view->with('alarmData', $alarm[0]);
+                }
+            }
         });
     }
 }
