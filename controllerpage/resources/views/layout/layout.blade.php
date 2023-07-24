2192f182fa91354e4641e657b94bb217ea33d1df
kbj95 <gwonbongjeong@gmail.com>
2023-07-24 14:45:07 +0900

    0724 layout _bj

--- controllerpage/resources/views/layout/layout.blade.php
+++ controllerpage/resources/views/layout/layout.blade.php
@@ -10,28 +10,113 @@
     <meta name="description"
         content="Monster Lite is powerful and clean admin dashboard template, inpired from Bootstrap Framework">
     <meta name="robots" content="noindex,nofollow">
+    <meta name="csrf-token" content="xe1t9is6-Q1bcuuJ8G5rdTXWCRqzkSat7FUI">
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css"/>
     <link rel="canonical" href="https://www.wrappixel.com/templates/monster-admin-lite/" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/temple/assets/images/favicon.png">
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">
     <link href="/temple/css/style.min.css" rel="stylesheet">
     @section('css')
     @show
 </head>
 <body>
-    @yield('contents')
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
+        data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+        <header class="topbar" data-navbarbg="skin6">
+            <nav class="navbar top-navbar navbar-expand-md navbar-dark">
+                <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle waves-effect waves-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="{{asset('temple/assets/images/users/2.jpg')}}" alt="user" class="profile-pic me-2">{{Auth::user()->mng_name}}
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="{{route('manager.logout')}}">로그아웃</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <aside class="left-sidebar" data-sidebarbg="skin6">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <!-- User Profile-->
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                            href="{{ route('member.memberlist')}}" aria-expanded="false">
+                            <i class="me-3 fa fa-user" aria-hidden="true"></i><span
+                                class="hide-menu">회원관리</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="{{route('user.food')}}" aria-expanded="false">
+                                <i class="me-3 fa-solid fa-utensils" aria-hidden="true"></i>
+                                <span class="hide-menu">회원 음식관리</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="{{route('manager.food')}}" aria-expanded="false"><i class="me-3 fa-solid fa-bowl-food"
+                                    aria-hidden="true"></i><span class="hide-menu">관리자 음식관리</span>
+                            </a>
+                        </li>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="{{route('board.boardlist')}}" aria-expanded="false"><i class="me-3 fa fa-table"
+                                    aria-hidden="true"></i><span class="hide-menu">게시글 관리</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="{{ route('comment.commentlist') }}" aria-expanded="false"><i class="me-3 fa fa-font"
+                                    aria-hidden="true"></i><span class="hide-menu">댓글 관리</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a class="sidebar-link waves-effect waves-dark sidebar-link"
+                                href="{{ route('report.get') }}" aria-expanded="false"><i class="me-3 fa-solid fa-circle-exclamation"
+                                    aria-hidden="true"></i><span class="hide-menu">신고 관리</span>
+                            </a>
+                        </li>
+                    </ul>
+
+                </nav>
+            </div>
+        </aside>
+        <div class="page-wrapper">
+            @yield('contents')
+            <footer class="footer text-center">
+                © 2021 Monster Admin by <a href="https://www.wrappixel.com/">wrappixel.com</a>
+            </footer>
+        </div>
+    </div>
+
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/e3754443e1.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://themes.getbootstrap.com/wp-content/themes/bootstrap-marketplace/assets/javascript/scripts.js?ver=1670173476"></script>
-    <script src="/temple/assets/plugins/jquery/dist/jquery.min.js"></script>
-    <script src="/temple/assets/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/temple/js/app-style-switcher.js"></script>
-    <script src="/temple/js/waves.js"></script>
-    <script src="/temple/js/sidebarmenu.js"></script>
-    <script src="/temple/js/custom.js"></script>
+    <script src="{{asset('temple/assets/plugins/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{asset('temple/assets/plugins/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('temple/js/app-style-switcher.js')}}"></script>
+    <script src="{{asset('temple/js/waves.js')}}"></script>
+    <script src="{{asset('temple/js/sidebarmenu.js')}}"></script>
+    <script src="{{asset('temple/js/custom.js')}}"></script>
+
     @yield('js')
     @include('sweetalert::alert')
 </body>
